dc12c42bf53a2cc3f74caec6a07d58ca41b27455
flying sheep <flying-sheep@web.de>
2013-01-12 00:49:48 +0100

    setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from distutils.core import setup
 
-setup(name = 'bcode',
-	version = '1.0',
-	py_modules = ['bcoding',],
-	description = 'bencoding & -decoding library',
-	author = 'Phil Schaf',
-	author_email = 'flying-sheep@web.de',
-	license = 'MIT',
-	url = 'http://flying-sheep.github.com/bcode')
+setup(
+	name='bcoding',
+	version='1.0',
+	py_modules=['bcoding',],
+	description='bencoding & -decoding library',
+	author='Phil Schaf',
+	author_email='flying-sheep@web.de',
+	license='MIT',
+	url='http://flying-sheep.github.com/bcode')
