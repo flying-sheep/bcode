32a8b152611fac262ccdafca46c1d07c2719a266
flying sheep <flying-sheep@web.de>
2013-01-11 19:10:36 +0100

    renamed project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name = 'bcode',
+	version = '1.0',
+	py_modules = ['bcoding',],
+	description = "bencoding & -decoding library",
+	author = "Phil Schaf",
+	author_email = "flying-sheep@web.de",
+	license = 'MIT',
+	url = "http://flying-sheep.github.com/bcoding/")
